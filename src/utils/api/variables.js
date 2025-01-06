8510b333e7af8cfaccbe75191e91490171d07782
dinhnq-gihot <dinh.nguyen@gihot.vn>
2025-01-06 10:23:49 +0700

    chore(ci-cd): adjust domain backend

--- src/utils/api/variables.js
+++ src/utils/api/variables.js
@@ -1,8 +1,8 @@
 export const variables = {
-    BASE_URL: "http://10.148.0.7:5214/api/",
-    USER_API: "http://10.148.0.7:5214/api/user",
-    PRODUCT_API: "http://10.148.0.7:5214/api/product",
-    ORDER_API: "http://10.148.0.7:5214/api/order",
-    PRODUCTSIZE_API: "http://10.148.0.7:5214/api/productsize",
-    ORDERITEM_API: "http://10.148.0.7:5214/api/orderitem",
+    BASE_URL: "http://34.126.130.233:5214/api/",
+    USER_API: "http://34.126.130.233:5214/api/user",
+    PRODUCT_API: "http://34.126.130.233:5214/api/product",
+    ORDER_API: "http://34.126.130.233:5214/api/order",
+    PRODUCTSIZE_API: "http://34.126.130.233:5214/api/productsize",
+    ORDERITEM_API: "http://34.126.130.233:5214/api/orderitem",
 }

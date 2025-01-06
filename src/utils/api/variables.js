c7a02a5072a75abccfcd4acba64d669917a47479
dinhnq-gihot <dinh.nguyen@gihot.vn>
2025-01-07 01:12:51 +0700

    chore(pipeline): adjust api domain

--- src/utils/api/variables.js
+++ src/utils/api/variables.js
@@ -1,8 +1,8 @@
 export const variables = {
-    BASE_URL: "http://api-online-shop.devopsedu.vn:5214/api/",
-    USER_API: "http://api-online-shop.devopsedu.vn:5214/api/user",
-    PRODUCT_API: "http://api-online-shop.devopsedu.vn:5214/api/product",
-    ORDER_API: "http://api-online-shop.devopsedu.vn:5214/api/order",
-    PRODUCTSIZE_API: "http://api-online-shop.devopsedu.vn:5214/api/productsize",
-    ORDERITEM_API: "http://api-online-shop.devopsedu.vn:5214/api/orderitem",
+    BASE_URL: "http://api-online-shop.devopsedu.vn/api/",
+    USER_API: "http://api-online-shop.devopsedu.vn/api/user",
+    PRODUCT_API: "http://api-online-shop.devopsedu.vn/api/product",
+    ORDER_API: "http://api-online-shop.devopsedu.vn/api/order",
+    PRODUCTSIZE_API: "http://api-online-shop.devopsedu.vn/api/productsize",
+    ORDERITEM_API: "http://api-online-shop.devopsedu.vn/api/orderitem",
 }

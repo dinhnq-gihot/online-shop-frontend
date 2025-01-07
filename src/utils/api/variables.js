674f1499e785372764d343f62236f0b4c7e1ab76
dinhnq-gihot <dinh.nguyen@gihot.vn>
2025-01-07 21:22:47 +0700

    chore(pipeline): change api domain 35.186.147.154

--- src/utils/api/variables.js
+++ src/utils/api/variables.js
@@ -1,8 +1,8 @@
 export const variables = {
-    BASE_URL: "http://34.126.130.233:5214/api/",
-    USER_API: "http://34.126.130.233:5214/api/user",
-    PRODUCT_API: "http://34.126.130.233:5214/api/product",
-    ORDER_API: "http://34.126.130.233:5214/api/order",
-    PRODUCTSIZE_API: "http://34.126.130.233:5214/api/productsize",
-    ORDERITEM_API: "http://34.126.130.233:5214/api/orderitem",
+    BASE_URL: "http://35.186.147.154:5214/api/",
+    USER_API: "http://35.186.147.154:5214/api/user",
+    PRODUCT_API: "http://35.186.147.154:5214/api/product",
+    ORDER_API: "http://35.186.147.154:5214/api/order",
+    PRODUCTSIZE_API: "http://35.186.147.154:5214/api/productsize",
+    ORDERITEM_API: "http://35.186.147.154:5214/api/orderitem",
 }
